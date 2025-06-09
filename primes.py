9aaae9d34de2a2e88ea1f27f2db96ffa205afe87
penguyen72 <peytonn72@gmail.com>
2025-06-08 23:28:21 -0700

    add log polling

--- primes.py
+++ primes.py
@@ -21,9 +21,9 @@ class PrimeFinder:
         return True
 
     def find(self):
+        i = 1
         while self.running:
             self.condition.acquire()
-            i = 1
             while not self.is_prime(i):
                 i += 1
 
@@ -31,6 +31,7 @@ class PrimeFinder:
             self.found = True
             self.prime_num = i
             self.condition.release()
+            i += 1
 
     def log(self):
         while self.running:
@@ -56,6 +57,58 @@ class PrimeFinder:
         log_thread.join()
 
 
+class PollingPrimeFinder:
+    def __init__(self):
+        self.running = True
+        self.found = False
+        self.prime_num = None
+
+    def is_prime(self, num):
+        if num == 2 or num == 3:
+            return True
+        div = 2
+        while div <= num / 2:
+            if num % div == 0:
+                return False
+            div += 1
+        return True
+
+    def find(self):
+        i = 1
+        while self.running:
+            while not self.is_prime(i):
+                i += 1
+            self.prime_num = i
+            self.found = True
+
+            time.sleep(0.5)
+
+            i += 1
+
+    def log(self):
+        while self.running:
+            time.sleep(0.5)
+
+            if self.found:
+                print(self.prime_num)
+                self.prime_num = None
+                self.found = False
+
+    def run(self):
+        find_thread = Thread(target=self.find)
+        log_thread = Thread(target=self.log)
+
+        find_thread.start()
+        log_thread.start()
+
+        time.sleep(3)
+        self.running = False
+
+        find_thread.join()
+        log_thread.join()
+
+
 if __name__ == "__main__":
+    pollingPrimeFinder = PollingPrimeFinder()
     primeFinder = PrimeFinder()
-    primeFinder.run()
+    pollingPrimeFinder.run()
